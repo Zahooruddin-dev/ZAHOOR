969e685a2a2e9d841bad54d606c97e4b087a67a5
Zahooruddin-dev <84314402+Zahooruddin-dev@users.noreply.github.com>
2023-02-06 19:28:21 +0500

    Payment issue update

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://ai2-qp8w.onrender.com', {
+    const response = await fetch('https://ai-vqwu.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
